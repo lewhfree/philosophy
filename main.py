d96b33b3f4c0d705351d37906114d7acb6266e7a
lewhfree <>
2025-05-06 20:20:53 -0700

    Cleaned up a littl
    Going to encapsulate it later so that i can mass scrape the pages

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import requests
 
 from bs4 import BeautifulSoup
 
+def getRandomTitle():
+    url = "https://en.wikipedia.org/w/api.php"
+
+    params = {
+        "action": "query",
+        "format": "json",
+        "list": "random",
+        "rnlimit": "1"
+    }
+
+    request = requests.get(url=url, params=params)
+    data = request.json()
+
+    random = data["query"]["random"]
+    for r in random:
+       return r["title"] 
+
 def getLink(title):
     url = f"https://en.wikipedia.org/api/rest_v1/page/html/{title}"
     headers = {"User-Agent": "philosophyFinder/0.0.1 (github.com/lewhfree/philosophy)"}
@@ -15,13 +32,26 @@ def getLink(title):
                 return a['href'].split("/")[1]
     return None
 
+seen = set()
+
 target = "Philosophy"
 
-start = "Bogosort"
+start = "Cuttlefish"
+
+start = getRandomTitle()
+
 
 current = start
 print(current)
+seen.add(current)
 
 while target != current:
     current = getLink(current)
     print(current)
+    if current in seen:
+        print("Seen before, loop")
+        exit()
+    else:
+        seen.add(current)
+
+print("Seen " + str(len(seen)))
