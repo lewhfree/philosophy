c3ad08fdf3e2885dceb6bf5234c24de4e2303200
lewhfree <>
2025-05-07 17:51:30 -0700

    i hate c i hate c i hate c i hate c i hate c

--- c_rewrite/main.c
+++ c_rewrite/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
+#include <curl/curl.h>
+#include <string.h>
 
-char request(char page){
-  return page;
+CURLcode nextLink(char * page){
+  char* url = "https://en.wikipedia.org/api/rest_v1/page/html/Bogosort";
+  CURL *curl = curl_easy_init();
+  if(curl){
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    res = curl_easy_perform(curl);
+    curl_easy_cleanup(curl);
+
+    return res;
+  }
+ 
 }
 
 int main(){
-  printf("Printing hooray!");
+  char * text = (char *) nextLink("Thing");
+  
+  char * href_start = strstr(text, "<a ");
+  if (href_start){
+    printf("Found a at:i \n");
+    printf((char *) href_start);
+  }
   return 1;
 }
